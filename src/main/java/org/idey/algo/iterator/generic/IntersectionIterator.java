e0ac37b855316d103ad1189cf80e81212782897c
Indranil Dey <idey@roku.com>
2016-06-06 08:15:28 -0700

    commit

--- src/main/java/org/idey/algo/iterator/generic/IntersectionIterator.java
+++ src/main/java/org/idey/algo/iterator/generic/IntersectionIterator.java
@@ -24,18 +24,15 @@ public class IntersectionIterator<T extends Comparable<T>> implements Iterator<T
             }
         }
         this.iterators = iterators;
-        this.comparator = new Comparator<T>() {
-            @Override
-            public int compare(T o1, T o2) {
-                if(o1==null && o2==null){
-                    return 0;
-                }else if(o1==o2){
-                    return 0;
-                }else if(o1==null){
-                    return 1;
-                }else{
-                    return -o1.compareTo(o2);
-                }
+        this.comparator = (o1, o2) -> {
+            if(o1==null && o2==null){
+                return 0;
+            }else if(o1==o2){
+                return 0;
+            }else if(o1==null){
+                return 1;
+            }else{
+                return -o1.compareTo(o2);
             }
         };
         setAdvance();
